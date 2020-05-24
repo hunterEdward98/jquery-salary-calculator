50fdf8d74545ea0d51bd4f753bf24d3c32bf6fb9
Hunter Edward <Hunter.scheel@outlook.com>
2020-05-24 00:20:04 -0500

    Everything working. Submitting this commit

--- scripts/client.js
+++ scripts/client.js
@@ -4,36 +4,41 @@ function setUp() {
 
     $("#submitEmployeeBtn").on("click", function () {
         let tr = $('<tr></tr>');
-        tr.append(`<th>${$('#fNameIn').val()}</th>`);
+        tr.append(`<td>${$('#fNameIn').val()}</th>`);
         $('#fNameIn').val('');
-        tr.append(`<th>${$('#lNameIn').val()}</th>`);
+        tr.append(`<td>${$('#lNameIn').val()}</th>`);
         $('#lNameIn').val('');
-        tr.append(`<th>${$('#idIn').val()}</th>`);
+        tr.append(`<td>${$('#idIn').val()}</th>`);
         $('#idIn').val('');
-        tr.append(`<th>${$('#jTitleIn').val()}</th>`);
+        tr.append(`<td>${$('#jTitleIn').val()}</th>`);
         $('#jTitleIn').val('');
-        tr.append(`<th>${$('#salaryIn').val()}</th>`);
+        tr.append(`<td>${$('#salaryIn').val()}</th>`);
         totalSalary += Number($('#salaryIn').val());
         $('#salaryIn').val('');
         let delBtn = $(`<th class = ><button class = "delete">Delete</button></th>`);
         tr.append(delBtn);
         console.log(tr);
         $("tbody").append(tr);
+        $(".deleteAll").on('click', function () {
+            $('tbody').empty();
+            totalSalary = 0;
+            $('#total').empty();
+            $('#total').append('Total Monthly Cost: 0');
+        });
         $(`.delete`).on('click', function (event) {
             let rmSal = $(this).parent().siblings(':last').text();
             totalSalary -= rmSal;
             $(this).parent().parent().remove();
-            if(totalSalary/12 <= 20000){
-                $(`#total`).css(`background-color`,`#c0c0c0`);
-            }$('#total').empty();
-            $('#total').append('Total Monthly Cost: '+totalSalary/12);
+            if (totalSalary / 12 <= 20000) {
+                $(`#total`).css(`background-color`, `#c0c0c0`);
+            } $('#total').empty();
+            $('#total').append('Total Monthly Cost: ' + totalSalary / 12);
         });
-        if (totalSalary/12 > 20000) {
+        if (totalSalary / 12 > 20000) {
             $(`#total`).css('background-color', 'red');
         }
         $('#total').empty();
-        $('#total').append('Total Monthly Cost: '+totalSalary/12);
+        $('#total').append('Total Monthly Cost: ' + totalSalary / 12);
     });
-    $(`.content`).append(`<h3 id = 'total'>Total Monthly Cost: ${totalSalary/12}<h3>`);
-
+    $(`.content`).append(`<h3 id = 'total'>Total Monthly Cost: ${totalSalary / 12}<h3>`);
 }
