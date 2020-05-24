0d64d4404cb720c570b72c6d6e75ee931be9caac
Hunter Edward <Hunter.scheel@outlook.com>
2020-05-23 22:04:58 -0500

    made fields empty on submission

--- scripts/client.js
+++ scripts/client.js
@@ -5,10 +5,15 @@ function setUp(){
     $("#submitEmployeeBtn").on("click", function(){
         tr = $('<tr></tr>');
         tr.append(`<th>${$('#fNameIn').val()}</th>`);
+        $('#fNameIn').val('');
         tr.append(`<th>${$('#lNameIn').val()}</th>`);
+        $('#lNameIn').val('');
         tr.append(`<th>${$('#idIn').val()}</th>`);
+        $('#idIn').val('');
         tr.append(`<th>${$('#jTitleIn').val()}</th>`);
+        $('#jTitleIn').val('');
         tr.append(`<th>${$('#salaryIn').val()}</th>`);
+        $('#salaryIn').val('');
         tr.append(`<th><button>Delete</button></th>`);
         console.log(tr);
         $("tbody").append(tr);
