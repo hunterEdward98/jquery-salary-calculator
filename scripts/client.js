0bdd3f6cf9d418bf8cb2fa02c04ccc46d8cbe44d
Hunter Edward <Hunter.scheel@outlook.com>
2020-05-23 22:46:49 -0500

    Delete button deletes employee and salary

--- scripts/client.js
+++ scripts/client.js
@@ -1,9 +1,10 @@
 $(document).ready(setUp);
+let totalSalary = 0;
 function setUp(){
 
-
+debugger;
     $("#submitEmployeeBtn").on("click", function(){
-        tr = $('<tr></tr>');
+        let tr = $('<tr></tr>');
         tr.append(`<th>${$('#fNameIn').val()}</th>`);
         $('#fNameIn').val('');
         tr.append(`<th>${$('#lNameIn').val()}</th>`);
@@ -13,9 +14,16 @@ function setUp(){
         tr.append(`<th>${$('#jTitleIn').val()}</th>`);
         $('#jTitleIn').val('');
         tr.append(`<th>${$('#salaryIn').val()}</th>`);
+        totalSalary+=$('#salaryIn').val();
         $('#salaryIn').val('');
-        tr.append(`<th><button>Delete</button></th>`);
+        let delBtn = $(`<th class = ><button class = "delete">Delete</button></th>`);
+        tr.append(delBtn);
         console.log(tr);
         $("tbody").append(tr);
+        $(`.delete`).on('click',function(event){
+            let rmSal = $(this).parent().siblings(':last').text();
+            totalSalary-=rmSal;
+            $(this).parent().parent().remove();
+        });
     });
 }
