cd271470edc4928780bf85a51ba241807090d58a
Hunter Edward <Hunter.scheel@outlook.com>
2020-05-24 01:28:58 -0500

    finished formatting and commenting

--- scripts/client.js
+++ scripts/client.js
@@ -4,6 +4,8 @@ function setUp() {
 
     $("#submitEmployeeBtn").on("click", function () {
         let tr = $('<tr></tr>');
+
+        //append the input values to the table, then clear them.
         tr.append(`<td>${$('#fNameIn').val()}</th>`);
         $('#fNameIn').val('');
         tr.append(`<td>${$('#lNameIn').val()}</th>`);
@@ -15,28 +17,41 @@ function setUp() {
         tr.append(`<td>${$('#salaryIn').val()}</th>`);
         totalSalary += Number($('#salaryIn').val());
         $('#salaryIn').val('');
-        let delBtn = $(`<th class = ><button class = "delete">Delete</button></th>`);
-        tr.append(delBtn);
+
+        //add a delete 'button'
+        tr.append(`<td class = "delete">X</td>`);
         console.log(tr);
+
+        //add your tr to the table body
         $("tbody").append(tr);
+
+        //if your Delete All button is clicked, handle the click
         $(".deleteAll").on('click', function () {
             $('tbody').empty();
             totalSalary = 0;
             $('#total').empty();
             $('#total').append('Total Monthly Cost: 0');
         });
+
+        //handle the delete button you just added.
         $(`.delete`).on('click', function (event) {
-            let rmSal = $(this).parent().siblings(':last').text();
+            let rmSal = $(this).siblings(':last').text();
             totalSalary -= rmSal;
-            $(this).parent().parent().remove();
+            $(this).parent().remove();
+
+            //update total Salary
             if (totalSalary / 12 <= 20000) {
                 $(`#total`).css(`background-color`, `#c0c0c0`);
             } $('#total').empty();
             $('#total').append('Total Monthly Cost: ' + totalSalary / 12);
         });
+
+        //handle total salary bg color
         if (totalSalary / 12 > 20000) {
             $(`#total`).css('background-color', 'red');
         }
+
+        //update total salary
         $('#total').empty();
         $('#total').append('Total Monthly Cost: ' + totalSalary / 12);
     });
